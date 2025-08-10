cdec3a307ca554d6df8e1b7d35ada8c37c3b408d
vinzz076 <ahmadfaridwahyu@gmail.com>
2025-08-10 22:20:41 +0700

    Add files via upload
    VinzzDev

---
+++ password.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "VinzzXVermeil",
+
+  "description": "Vermeil Multi Device", 
+
+  "lastUpdated": "10/08/2025",
+
+  "author": "VinzzDev", 
+
+  "status": "Sistem Active!",
+
+  "license": "MIT",
+
+  "version": "1.0"
+
+    }
